b03b4578287f2ef79750901eeccbd0b2bfffda14
SIIMON20 <114408702+SIIMON20@users.noreply.github.com>
2024-02-12 19:43:00 -0500

    Template listo sin rutas

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -9,18 +9,27 @@ export class InfoPaginaService {
 
   info : InfoPagina = {};
   cargada = false
+  equipo : any[] = [];
 
   constructor( private http: HttpClient ) { 
-    console.log("Servicio de página lanzado")
+    this.cargarEquipo();
+    this.cargarInfo();
+  }
 
+  private cargarInfo(){
     //Leer el archivo JSON
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp:InfoPagina) => {
         this.cargada = true;
         this.info = resp;
-        //console.log(resp);
-        console.log( resp);
 
       });
   }
+  private cargarEquipo(){
+    this.http.get('https://angular-html-8d9a7-default-rtdb.firebaseio.com/equipo.json')
+      .subscribe( (resp :any) => {
+        this.equipo = resp;
+      });
+  }
+
 }
